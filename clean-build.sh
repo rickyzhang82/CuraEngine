55f8dcffdad24e84e027ecd7c2daffb400526b63
Ricky Zhang <rickyzhang@gmail.com>
2018-05-26 09:57:27 -0400

    Print out build script message
    Signed-off-by: Ricky Zhang <rickyzhang@gmail.com>

--- clean-build.sh
+++ clean-build.sh
@@ -9,13 +9,16 @@ echo "Entering build/ folder"
 
 if [ "$1" == "debug" ];
     then
+        echo "Build for debug with G3LOG"
         cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_G3LOG=ON ..
         make -j 8
 elif [ "$1" == "production" ]; 
     then
-        cmake -DCMAKE_BUILD_TYPE=Release ..
+        echo "Build for production"
+        cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_G3LOG=OFF ..
         make VERBOSE=1 -j 8
 else
+        echo "Build for releaase with G3LOG"
         cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_G3LOG=ON ..
         make VERBOSE=1 -j 8
 fi
